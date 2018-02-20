c4a5c27fbc2291300d51f4909ed1bfd5bf6c055a
Alexander Neumann and Tim Firman <alexander@>
2018-02-20 10:10:53 -0800

    Fix git repository.

---
+++ Inventory.Tests/ModelTests/InventoryTests.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Inventory.Models;
+namespace Inventory.Tests
+{
+    [TestClass]
+    public class InventoryTest
+    {
+        [TestMethod]
+        public void InventoryTester()
+        {
+        }
+      }
+}
