3b88f8784f9024f41496ebfa3514111042e9993c
Guest User <Guest@epicodus-15.local>
2018-02-27 15:54:42 -0800

    adds more things.

--- SimpleCart/Controllers/UserController.cs
+++ SimpleCart/Controllers/UserController.cs
@@ -10,9 +10,10 @@ namespace SimpleCart.Controllers
     public class UserController : Controller
     {
 
-        [HttpGet("/User/Form")]
-        public ActionResult Form()
+        [HttpGet("/User/Form/{sessionId}")]
+        public ActionResult Form(int sessionId)
         {
+          ViewBag.sessionId = sessionId; 
           return View();
         }
 
@@ -25,9 +26,13 @@ namespace SimpleCart.Controllers
             string address = Request.Form["addressForm"];
             string email = Request.Form["emailForm"];
             AppUser newUser = new AppUser(name, username, password, address, email);
-            newUser.Save();
-
-            return RedirectToAction("Display", "Item");
+            if (newUser.Save())
+            {
+              string login = username;
+              int sessionId = AppUser.Login(login, password);
+              return RedirectToAction("Display", "Item", new {id=sessionId});
+            }
+            return RedirectToAction("Form", new {id=-1});
         }
 
         [HttpGet("/User/Login")]
