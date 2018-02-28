24abd88b3bf1a8433446101258a5a82754c9668e
Manuela Garcia <manuela.garcia.dev@gmail.com>
2018-02-27 16:12:44 -0800

    Debug Order By Method.

--- SimpleCart/Controllers/ItemController.cs
+++ SimpleCart/Controllers/ItemController.cs
@@ -12,26 +12,25 @@ namespace SimpleCart.Controllers
         [HttpGet("/Item/{id}")]
         public ActionResult Display(int id)
         {
+            string inputOrderBy = "id";
             ViewBag.sessionId = id;
             if (id != -1)
             {
-              Cart myCart = new Cart(id);
-              int myUserId = myCart.GetUserId();
-              AppUser myUser = AppUser.Find(myUserId);
-              Console.WriteLine(myUser.GetName());
-              ViewBag.myUserName = myUser.GetName();
-              return View("AllItems", Item.GetAll("id"));
+                Cart myCart = new Cart(id);
+                int myUserId = myCart.GetUserId();
+                AppUser myUser = AppUser.Find(myUserId);
+                Console.WriteLine(myUser.GetName());
+                ViewBag.myUserName = myUser.GetName();
+                return View("AllItems", Item.GetAll("id"));
             }
-            return View("AllItems", Item.GetAll("id"));
-        }
-
-        [HttpPost("/Item")]
-        public ActionResult DisplaySort()
-        {
-            string inputOrderBy = Request.Form["orderBy"];
+            if(!string.IsNullOrEmpty(Request.Query["orderBy"]))
+            {
+                inputOrderBy = Request.Query["orderBy"];
+            }
 
             return View("AllItems", Item.GetAll(inputOrderBy));
         }
+
 
         [HttpGet("/Item/Detail/{itemId}/{sessionId}")]
         public ActionResult Detail(int itemId, int sessionId)
